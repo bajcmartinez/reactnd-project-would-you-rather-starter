a431c678a56cafb8b975f5d47aafcf262c31b9a5
Juan Cruz Martinez <bajcmartinez@gmail.com>
2018-10-24 17:17:33 -0400

    feat: show a message when no questions are available on the list

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -5,6 +5,14 @@ import './QuestionsList.module.scss';
 import QuestionCard from "./QuestionCard";
 
 export default function QuestionList(props) {
+    if (props.questions && props.questions.length === 0) {
+        return (
+            <div>
+                All done, come back later to answer some more questions
+            </div>
+        )
+    }
+
     return (
        <div>
            <TransitionGroup>
