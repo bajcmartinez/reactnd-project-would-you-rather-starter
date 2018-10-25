adbc0cad59c2867c6ac8ddbd9a8d55e5e0133d5b
Juan Cruz Martinez <bajcmartinez@gmail.com>
2018-10-24 23:00:07 -0400

    refactor: UserCard is now a stateless functional component

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import PropTypes from 'prop-types';
 import { Card, ListGroup, Badge } from 'react-bootstrap';
 
@@ -7,57 +7,56 @@ import * as style from './QuestionCard.module.scss';
 const special = ['zeroth','first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
 const deca = ['twent', 'thirt', 'fort', 'fift', 'sixt', 'sevent', 'eight', 'ninet'];
 
-class UserCard extends Component {
+const UserCard = (props) => {
 
-    stringifyNumber(n) {
+    const stringifyNumber = (n) => {
         if (n < 20) return special[n];
         if (n%10 === 0) return deca[Math.floor(n/10)-2] + 'ieth';
         return deca[Math.floor(n/10)-2] + 'y-' + special[n%10];
-    }
+    };
 
-    render() {
-        const { user, position } = this.props;
+    const { user, position } = props;
 
-        const askedQuestions = user.questions.length;
-        const answeredQuestions = Object.keys(user.answers).length;
+    const askedQuestions = user.questions.length;
+    const answeredQuestions = Object.keys(user.answers).length;
 
-        return (
-            <Card bg='info' text="white" className={style.question}>
-                <Card.Body className={style.cardMain}>
-                    <small>{this.stringifyNumber(position).toUpperCase()}</small>
-                    <Card.Title className="d-flex justify-content-between align-items-center">
-                        <span>
-                            <img alt={user.name} src={user.avatarURL} className="img-thumbnail" />
-                            &nbsp;
-                            {user.name}
-                        </span>
-                        <span className="text-center">
-                            Points:
-                            <h1>
-                                <Badge variant="success">{askedQuestions+answeredQuestions}</Badge>
-                            </h1>
-                        </span>
-                    </Card.Title>
-                    <Card.Text>
+    return (
+        <Card bg='info' text="white" className={style.question}>
+            <Card.Body className={style.cardMain}>
+                <small>{stringifyNumber(position).toUpperCase()}</small>
+                <Card.Title className="d-flex justify-content-between align-items-center">
+                    <span>
+                        <img alt={user.name} src={user.avatarURL} className="img-thumbnail" />
+                        &nbsp;
+                        {user.name}
+                    </span>
+                    <span className="text-center">
+                        Points:
+                        <h1>
+                            <Badge variant="success">{askedQuestions+answeredQuestions}</Badge>
+                        </h1>
+                    </span>
+                </Card.Title>
+                <Card.Text>
 
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup variant="flush">
-                    <ListGroup.Item variant='info' className="d-flex justify-content-between align-items-center">
-                        Asked questions <Badge variant="secondary">{askedQuestions}</Badge>
-                    </ListGroup.Item>
-                    <ListGroup.Item variant='info' className="d-flex justify-content-between align-items-center">
-                        Answered questions <Badge variant="secondary">{answeredQuestions}</Badge>
-                    </ListGroup.Item>
-                </ListGroup>
-            </Card>
-        )
-    }
-}
+                </Card.Text>
+            </Card.Body>
+            <ListGroup variant="flush">
+                <ListGroup.Item variant='info' className="d-flex justify-content-between align-items-center">
+                    Asked questions <Badge variant="secondary">{askedQuestions}</Badge>
+                </ListGroup.Item>
+                <ListGroup.Item variant='info' className="d-flex justify-content-between align-items-center">
+                    Answered questions <Badge variant="secondary">{answeredQuestions}</Badge>
+                </ListGroup.Item>
+            </ListGroup>
+        </Card>
+    )
+};
+
+UserCard.propTypes = {
+    position: PropTypes.number.isRequired,
+    user: PropTypes.object.isRequired
+};
 
 export default UserCard;
 
-UserCard.propTypes = {
-  position: PropTypes.number.isRequired,
-  user: PropTypes.object.isRequired
-}
